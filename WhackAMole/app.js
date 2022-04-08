7d77d897e4551fbc8074d6ded0bb814880103930
Renee <97166586+reneeatho@users.noreply.github.com>
2022-04-08 14:38:58 +0200

    Add files via upload

---
+++ WhackAMole/app.js
@@ -0,0 +1,45 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole');
+    })
+    let randomPosition = square[Math.floor((Math.random() *9))]
+    randomPosition.classList.add('mole');
+
+
+hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition){
+            result = result + 1
+            score.textContent = result
+        }
+    })
+})
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+  
+}
+moveMole();
+
+function countdown() {
+    currentTime --
+    timeLeft.textContent = currentTime
+    if(currentTime === 0 ) {
+        clearInterval(timerId)
+        alert('GAME OVER! Antall poeng:' + result)
+    }
+}
+
+let timerId = setInterval(countdown, 1000)
